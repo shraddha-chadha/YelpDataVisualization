d9d4787be9401ca8580ae0405e8cdcfc51ef549d
Shraddha Chadha <shradhachadha08@gmail.com>
2019-05-08 22:20:15 -0700

    React setup

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
@@ -8,5 +10,5 @@ app.get('/', function (req, res) {
 var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Example app listening at http://127.0.0.1:8081", host, port)
 })
