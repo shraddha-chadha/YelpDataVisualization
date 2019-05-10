255d6cf177914ccffb5d52440545bec01c815f59
shraddha-chadha <shradhachadha08@gmail.com>
2019-05-10 18:51:34 +0000

    Added mongo db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mongo = require('mongodb');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -8,7 +9,5 @@ app.get('/', function (req, res) {
 });
 
 var server = app.listen(8081, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Example app listening at http://127.0.0.1:8081", host, port)
-})
+    console.log("Example app listening at http://127.0.0.1:8081");
+});
